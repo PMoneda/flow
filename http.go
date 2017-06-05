2b145dc42fdcbec2188d4b99583e0fd6c6f67aeb
Philippe Moneda <philippe.moneda@mundipagg.com>
2017-06-05 16:58:33 -0300

    :bug: bugfix when we have a http error during flow execution

--- http.go
+++ http.go
@@ -42,7 +42,7 @@ func getClient(skip string) *http.Client {
 	}*/
 	return client
 }
-func httpConnector(next func(), e *ExchangeMessage, out Message, u URI, params ...interface{}) error {
+func httpConector(next func(), e *ExchangeMessage, out Message, u URI, params ...interface{}) error {
 
 	newData := NewExchangeMessage()
 	var skip string
@@ -69,7 +69,10 @@ func httpConnector(next func(), e *ExchangeMessage, out Message, u URI, params .
 		body = strings.NewReader(t)
 	default:
 		if j, err := json.Marshal(b); err != nil {
-			panic(err)
+			newData.SetHeader("error", err.Error())
+			newData.SetBody(err)
+			out <- newData
+			next()
 		} else {
 			body = strings.NewReader(string(j))
 		}
@@ -77,6 +80,7 @@ func httpConnector(next func(), e *ExchangeMessage, out Message, u URI, params .
 	req, err = http.NewRequest(method, u.raw, body)
 
 	if err != nil {
+		newData.SetHeader("error", err.Error())
 		newData.SetBody(err)
 		out <- newData
 		next()
@@ -94,12 +98,18 @@ func httpConnector(next func(), e *ExchangeMessage, out Message, u URI, params .
 	req.Close = true
 	resp, errResp := client.Do(req)
 	if errResp != nil {
+		newData.SetHeader("error", errResp.Error())
+		newData.SetBody(errResp)
+		out <- newData
 		next()
 		return errResp
 	}
 	defer resp.Body.Close()
 	data, errResponse := ioutil.ReadAll(resp.Body)
 	if errResponse != nil {
+		newData.SetHeader("error", errResponse.Error())
+		newData.SetBody(errResponse)
+		out <- newData
 		next()
 		return errResponse
 	}
